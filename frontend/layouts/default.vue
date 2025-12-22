83421c4aba6280d316ae4ff8abdabc9350e60522
Patin Muangjan <patin@patin.dev>
2025-12-22 09:21:32 +0700

    FRONTEND: force drawer to use CSR Only

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,38 +1,39 @@
 <!-- layouts/dashboard.vue -->
 <template>
   <v-app>
-    <!-- Drawer -->
-    <v-navigation-drawer
-      v-model="drawer"
-      :temporary="smAndDown"
-      :rail="!smAndDown"
-      :expand-on-hover="!smAndDown"
-      scrim="false"
-      app
-    >
-    
-      <v-list density="compact" nav>
-        <v-list-item class="pt-1" :title="auth.user?.name || 'ไม่ระบุชื่อ'" :subtitle="auth.user?.email || '-'" prepend-icon="mdi-account"/>
-        <template v-for="sec in menu" :key="sec.label">
-          <v-divider class="my-2" />
-          <template v-for="it in sec.items" :key="it.to || it.href">
-            <NuxtLink
-              v-if="it.to"
-              :to="it.to"
-              class="no-underline"
-              color="surface"
-              exact-active-class="rounded-lg"
-            >
-              <v-list-item :title="it.label">
-                <template #prepend>
-                  <v-icon :icon="it.icon || 'mdi-circle-small'" />
-                </template>
-              </v-list-item>
-            </NuxtLink>
-          </template>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
+      <ClientOnly>
+        <v-navigation-drawer
+          v-model="drawer"
+          :temporary="smAndDown"
+          :rail="!smAndDown"
+          :expand-on-hover="!smAndDown"
+          scrim="false"
+          app
+        >
+        
+          <v-list density="compact" nav>
+            <v-list-item class="pt-1" :title="auth.user?.name || 'ไม่ระบุชื่อ'" :subtitle="auth.user?.email || '-'" prepend-icon="mdi-account"/>
+            <template v-for="sec in menu" :key="sec.label">
+              <v-divider class="my-2" />
+              <template v-for="it in sec.items" :key="it.to || it.href">
+                <NuxtLink
+                  v-if="it.to"
+                  :to="it.to"
+                  class="no-underline"
+                  color="surface"
+                  exact-active-class="rounded-lg"
+                >
+                  <v-list-item :title="it.label">
+                    <template #prepend>
+                      <v-icon :icon="it.icon || 'mdi-circle-small'" />
+                    </template>
+                  </v-list-item>
+                </NuxtLink>
+              </template>
+            </template>
+          </v-list>
+        </v-navigation-drawer>
+      </ClientOnly>
     <v-app-bar>
       <!-- <p></p> -->
       <v-toolbar-title class="font-semibold">ระบบประเมินบุคลากร</v-toolbar-title>
@@ -40,7 +41,7 @@
       <v-btn icon @click="toggleTheme" :aria-label="`Switch to ${themeName === 'dark' ? 'light':'dark'} theme`">
         <v-icon>mdi-theme-light-dark</v-icon>
       </v-btn>
-      <v-app-bar-nav-icon @click="logout()">
+      <v-app-bar-nav-icon @click="auth.logout()">
         <v-icon color="error">mdi-logout</v-icon>
       </v-app-bar-nav-icon>
     </v-app-bar>
@@ -110,12 +111,6 @@ function toggleTheme () {
 
 // Footer year
 const year = new Date().getFullYear()
-
-// Logout
-function logout () {
-  try { auth.logout() } catch {}
-  navigateTo('/login')
-}
 </script>
 
 <style scoped>
@@ -124,6 +119,4 @@ function logout () {
   font-size: 0.9rem;
   text-transform: uppercase;
 }
-
 </style>
-<!-- layouts/dashboard.vue -->
