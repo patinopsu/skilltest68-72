46b974b248c7e2cbbc4c07a7547c79f817799922
Patin Muangjan <patin@patin.dev>
2025-12-24 09:05:19 +0700

    BACKEND: remove comments

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -7,7 +7,6 @@ router.get('/',       auth('admin','evaluator','evaluatee'), ctrl.list);
 router.get('/:id',    auth('admin','evaluator','evaluatee'), ctrl.get);
 
 router.post('/',      auth('admin'), ctrl.create);
-// router.post('/',  ctrl.create);
 router.put('/:id',    auth('admin'), ctrl.update);
 router.delete('/:id', auth('admin'), ctrl.remove);
 
